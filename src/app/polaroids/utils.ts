98a500e2c4205f0e0f4d6f13bc0d33152779289d
astnai <aa@agustinarias.com>
2025-06-05 21:55:39 -0300

    pdates

--- src/app/polaroids/utils.ts
+++
@@ -1,5 +0,0 @@
-import { ROTATION_RANGE } from "./constants"
-
-export const getRotation = (id: number): number => {
-  return ((id * ROTATION_RANGE.multiplier) % (ROTATION_RANGE.max - ROTATION_RANGE.min)) + ROTATION_RANGE.min
-} 
